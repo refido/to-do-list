1aab268a4f7e8ece28091f1ce43c316451b6cade
revido <revidoberliano@gmail.com>
2022-10-26 20:44:25 +0700

    layout & add list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "react to do list.",
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+        type="text"
+        value={title}
+        onChange={(event) => {
+          setTitle(event.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          setTodos([...todos, { id: todos.length + 1, title: title }]);
+        }}
+      >
+        add
+      </button>
+
+      <h1>Todo List</h1>
+      <div className="todos-container">
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            {todo.title}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
